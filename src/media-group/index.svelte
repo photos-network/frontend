e509fb8ed8dd6f76add58e5a398c5dcba0f73b3c
Tomasz Borychowski <tborychowski@gmail.com>
2021-01-30 21:24:48 +0000

    move parallax from group to item

--- src/media-group/index.svelte
+++ src/media-group/index.svelte
@@ -1,7 +1,7 @@
 {#if group.title}
 	<h2 class="media-group-title">{group.title}</h2>
 {/if}
-<div class="media-group" bind:this="{elem}">
+<div class="media-group">
 	{#each group.items as item}
 		<MediaItem src="{item}" />
 	{/each}
@@ -12,35 +12,5 @@
 import MediaItem from '../media-item';
 
 export let group = { title: '', items: [] };
-let elem, instance;
-
-
-import { onDestroy, onMount } from 'svelte';
-import * as basicScroll from 'basicscroll';
-
-$:{
-	if (group.items.length) requestAnimationFrame(updateBasicScroll);
-}
-
-function updateBasicScroll () {
-	instance?.calculate();
-	instance?.update();
-}
-
-onMount(() => {
-	// create parallax/depth effect on photos (scroll them slower than the page)
-	instance = basicScroll.create({
-		elem,
-		from: 'top-bottom',
-		to: 'bottom-top',
-		direct: true,
-		props: { '--scroll-offset': { from: '0px', to: '60px' } }
-	});
-	instance.start();
-});
-
-onDestroy(() => {
-	instance?.destroy();
-});
 
 </script>
--- src/media-item/index.svelte
+++ src/media-item/index.svelte
@@ -1,6 +1,6 @@
 <a href="#{src.path}" class="media-item {src.type || 'photo'}"
 		class:loading="{loading}"
-		bind:this="{el}"
+		bind:this="{elem}"
 		on:click|preventDefault="{viewItem}"
 		id="{src.path}">
 	<img bind:this="{img}" src="#" alt="{src.name || ''}">
@@ -9,25 +9,41 @@
 
 
 <script>
-import { onMount } from 'svelte';
+import { onMount, onDestroy } from 'svelte';
 import { EVENT, inView } from '../lib';
+import * as basicScroll from 'basicscroll';
 import Spinner from '../spinner';
 export let src = { src: '', name: 'Loading...', type: 'photo' };
 
 let loading = false;
-let el, img;
+let elem, img, instance;
+
+function viewItem () {
+	EVENT.fire(EVENT.item.view, src, elem);
+}
+
 
 onMount(() => {
-	inView(el, () => {
+	// create parallax/depth effect on photos (scroll them slower than the page)
+	instance = basicScroll.create({
+		elem,
+		from: 'top-bottom',
+		to: 'bottom-top',
+		direct: true,
+		props: { '--scroll-offset': { from: '0px', to: '60px' } }
+	});
+	instance.start();
+
+
+	inView(elem, () => {
 		loading = true;
 		img.src = src.thumb || src.path || '#';
 		img.onload = () => requestAnimationFrame(() => loading = false);
 	});
 });
 
-
-function viewItem () {
-	EVENT.fire(EVENT.item.view, src, el);
-}
+onDestroy(() => {
+	instance?.destroy();
+});
 
 </script>
